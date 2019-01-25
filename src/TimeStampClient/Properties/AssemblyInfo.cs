c4c9398939fd8481226decc6e0fd435b6b510e31
Peter Polacko <peter.polacko@disig.sk>
2019-01-25 10:22:14 +0100

    Version bump to 1.0.1
    + copyright update

--- src/TimeStampClient/Properties/AssemblyInfo.cs
+++ src/TimeStampClient/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Disig a.s.")]
 [assembly: AssemblyProduct("TimeStampClient")]
-[assembly: AssemblyCopyright("Copyright 2016 Disig a.s.")]
+[assembly: AssemblyCopyright("Copyright 2016-2019 Disig a.s.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 #if DEBUG
 [assembly: InternalsVisibleTo("TimeStampClientTest")]
 #endif
