d7463616696c0e16399037524dfffd44903ce25b
Jaroslav Imrich <jimrich@jimrich.sk>
2021-02-07 19:49:31 +0100

    Updated Eto.Forms to 2.5.10
    - TimeStampClientGui targets .NET Framework 4.7.2
    - Deprecated Eto.Platform.Gtk2 and Eto.Platform.Gtk3 replaced with Eto.Platform.Gtk
    - Removed Eto.Platform.Windows because Eto.Platform.Wpf is enough

--- build/build_release.bat
+++ build/build_release.bat
@@ -42,20 +42,9 @@ mkdir %APPDIR% || goto :error
 mkdir %LICDIR% || goto :error
 
 @rem Copy binaries to the output directory
-copy ..\src\TimeStampClientGui\bin\Release\TimeStampClientGui.exe %APPDIR% || goto :error
-copy ..\src\TimeStampClientGui\bin\Release\TimeStampClientGui.exe.config %APPDIR% || goto :error
-copy ..\src\TimeStampClientGui\bin\Release\TimeStampClient.dll %APPDIR% || goto :error
-copy ..\src\TimeStampClientGui\bin\Release\BouncyCastle.Crypto.dll %APPDIR% || goto :error
-copy ..\src\TimeStampClientGui\bin\Release\Eto.Wpf.dll %APPDIR% || goto :error
-copy ..\src\TimeStampClientGui\bin\Release\Eto.WinForms.dll %APPDIR% || goto :error
-copy ..\src\TimeStampClientGui\bin\Release\MonoMac.dll %APPDIR% || goto :error
-copy ..\src\TimeStampClientGui\bin\Release\Eto.Mac64.dll %APPDIR% || goto :error
-copy ..\src\TimeStampClientGui\bin\Release\Eto.Gtk2.dll %APPDIR% || goto :error
-copy ..\src\TimeStampClientGui\bin\Release\Eto.Gtk3.dll %APPDIR% || goto :error
-copy ..\src\TimeStampClientGui\bin\Release\Eto.dll %APPDIR% || goto :error
-copy ..\src\TimeStampClientGui\bin\Release\Ionic.Zip.Reduced.dll %APPDIR% || goto :error
-copy ..\src\TimeStampClientCmd\bin\Release\TimeStampClientCmd.exe %APPDIR% || goto :error
-copy ..\src\TimeStampClientCmd\bin\Release\TimeStampClientCmd.exe.config %APPDIR% || goto :error
+copy ..\src\TimeStampClientGui\bin\Release\*.* %APPDIR% || goto :error
+del %APPDIR%\*.pdb
+del %APPDIR%\*.xml
 
 @rem Copy licenses to the output directory
 copy license\*.txt %LICDIR% || goto :error
