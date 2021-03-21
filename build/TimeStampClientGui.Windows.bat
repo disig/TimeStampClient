3a3c201074bc7fce8508fadea4186a826afee992
Jaroslav Imrich <jimrich@jimrich.sk>
2021-03-21 16:37:05 +0100

    Added cleanup to Gui.Windows build script

--- build/TimeStampClientGui.Windows.bat
+++ build/TimeStampClientGui.Windows.bat
@@ -18,6 +18,18 @@ setlocal
 call %tools%
 @echo on
 
+@rem Remove leftovers of any previous builds
+rmdir /S /Q ..\src\TimeStampClient\bin
+rmdir /S /Q ..\src\TimeStampClient\obj
+rmdir /S /Q ..\src\TimeStampClientCmd\bin
+rmdir /S /Q ..\src\TimeStampClientCmd\obj
+rmdir /S /Q ..\src\TimeStampClientGui.Linux\bin
+rmdir /S /Q ..\src\TimeStampClientGui.Linux\obj
+rmdir /S /Q ..\src\TimeStampClientGui.MacOs\bin
+rmdir /S /Q ..\src\TimeStampClientGui.MacOs\obj
+rmdir /S /Q ..\src\TimeStampClientGui.Windows\bin
+rmdir /S /Q ..\src\TimeStampClientGui.Windows\obj
+
 @rem Build release
 msbuild ..\src\TimeStampClientGui.Windows\TimeStampClientGui.Windows.csproj /p:Configuration=Release /target:Restore || goto :error
 msbuild ..\src\TimeStampClientGui.Windows\TimeStampClientGui.Windows.csproj /p:Configuration=Release /target:Clean || goto :error
